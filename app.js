aa7f96e80bf269c8a47d1daa5791b7b377ac7fd6
slowpoison36 <adhikaris1@hotmail.com>
2017-01-10 21:17:19 -0600

    prep repo for Heroku

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var app = express();
 const PORT= process.env.PORT||3000;
 app.use(express.static("public"))
 
-
+app.use(function(req,res,next){
+	if(req.headers["x-forwarded-proto"]==="http"){
+		next();
+	} else {
+		res.redirect("http://"+req.hostname+req.url);
+	}
+})
 
 
 app.listen(PORT,process.env.IP,function(){
